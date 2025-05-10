ddc5ce05085b8705ee702bcd45dc90eeb8b41a64
Ruaww06 <2985564406@qq.com>
2025-05-10 18:23:30 +0800

    上传代码文件

---
+++ utils.py
@@ -0,0 +1,17 @@
+# import os
+
+from langchain_openai import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+
+
+def get_chat_response(prompt, memory, openai_api_key):
+    model = ChatOpenAI(model="gpt-3.5-turbo", api_key=openai_api_key, base_url="https://api.aigc369.com/v1")
+    chain = ConversationChain(llm=model, memory=memory)
+
+    response = chain.invoke({"input": prompt})
+    return response["response"]
+
+memory = ConversationBufferMemory(return_messages=True)
+# print(get_chat_response("牛顿提出过那些知名的定律？", memory, os.getenv("OPENAI_API_KEY")))
+# print(get_chat_response("我上一个问题是什么？", memory, os.getenv("OPENAI_API_KEY")))
